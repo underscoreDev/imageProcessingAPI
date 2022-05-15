b55b2e88f94cfa77d5c78f52deeb618ee7b6ef9b
underscoreDev <godswill@zimmerman.team>
2022-05-15 19:23:13 +0100

    chore: changed to .test naming convention for test files

--- dist/tests/index.test.js
+++ dist/tests/index.test.js
@@ -5,5 +5,5 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const index_1 = __importDefault(require("../index"));
 it("expect myFunc(5) to equal 25", () => {
-    expect((0, index_1.default)(5)).toEqual(25);
+    expect((0, index_1.default)(5)).toEqual(26);
 });
--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,5 +1,5 @@
 import myFunc from "../index";
 
 it("expect myFunc(5) to equal 25", () => {
-  expect(myFunc(5)).toEqual(25);
+  expect(myFunc(5)).toEqual(26);
 });
