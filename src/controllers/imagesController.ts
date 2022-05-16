575a3573e0258da8b7863da4bd82c9141ab05ce5
underscoreDev <godswill@zimmerman.team>
2022-05-16 17:27:04 +0100

    feat: got api to work after building

--- src/controllers/imagesController.ts
+++ src/controllers/imagesController.ts
@@ -3,24 +3,33 @@ import { promises as fs } from "fs";
 import { NextFunction, Request, Response } from "express";
 
 export const checkUrl = (req: Request, res: Response, next: NextFunction) => {
-  if (!req.query.filename && !req.query.width && !req.query.height) {
-    return res.status(200).json({
-      message: "Failed",
-      data: "One of the following query parameters is missing (filename, width, height)",
-    });
+  try {
+    if (!req.query.filename && !req.query.width && !req.query.height) {
+      return res.status(400).json({
+        message: "Failed",
+        data: "One of the following query parameters is missing (filename, width, height)",
+      });
+    }
+    next();
+  } catch (error) {
+    throw error;
   }
-  next();
 };
 
 export const resizeImage = async (req: Request, _res: Response, next: NextFunction) => {
   try {
     const filename = req.query.filename;
-    console.log(filename);
     const width = Number(req.query.width);
     const height = Number(req.query.height);
-    const file = await fs.readFile(`assets/full/${filename}.jpg`);
+    const absPath = process.env.PWD?.replace("build", "");
 
-    await sharp(file).resize(width, height).toFormat("jpeg").toFile(`assets/thumb/${filename}.jpg`);
+    const file = await fs.readFile(`${absPath}/assets/full/${filename}.jpg`);
+
+    await sharp(file)
+      .resize(width, height)
+      .toFormat("jpeg")
+      .toFile(`${absPath}/assets/thumb/${filename}.jpg`);
+
     next();
   } catch (error) {
     throw error;
@@ -30,8 +39,9 @@ export const resizeImage = async (req: Request, _res: Response, next: NextFuncti
 export const sendImage = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const filename = req.query.filename;
+    const absPath = process.env.PWD?.replace("build", "");
 
-    const resizedImage = await fs.readFile(`assets/thumb/${filename}.jpg`);
+    const resizedImage = await fs.readFile(`${absPath}/assets/thumb/${filename}.jpg`);
 
     res.end(resizedImage);
     next();
--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,5 +1,1 @@
-import myFunc from "../index";
 
-it("expect myFunc(5) to equal 25", () => {
-  expect(myFunc(5)).toEqual(25);
-});
